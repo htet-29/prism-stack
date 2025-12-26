2247aceadab5e476b5cf512432c238765dc462cc
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2025-12-26 21:44:34 +0630

    Using httprouter package

---
+++ cmd/api/stocks.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) createStockHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new stock")
+}
+
+func (app *application) showStockHandler(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "showing the details of stock: %d\n", id)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/htet-29/prism-stack
 
 go 1.25.5
+
+require github.com/julienschmidt/httprouter v1.3.0
