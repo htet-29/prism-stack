0dab1d5a205cba66caf4f677d4d5561bac1a34cc
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2025-12-27 09:02:18 +0630

    Routing error

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,9 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/stocks", app.createStockHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/stocks/:id", app.showStockHandler)
