d1977a8e3d0dcfa13237028fd6322b8dec4860da
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-01-06 23:11:13 +0630

    decoupling dsn string

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
 var version = "1.0.0"
@@ -29,10 +30,13 @@ type application struct {
 
 func main() {
 	var cfg config
+	if err := godotenv.Load(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error loading .env file")
+	}
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://prism_stack:pa55word@localhost/prism_stack", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("PRISM_STACK_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
